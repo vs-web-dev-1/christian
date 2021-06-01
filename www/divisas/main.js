6c48c1f98dac8023fee238927a0da7b63e04a5a6
Christian Villar Doral <cvillar@vectoritcgroup.com>
2021-06-01 12:33:10 +0200

    Fechas.

--- www/divisas/main.js
+++ www/divisas/main.js
@@ -4,7 +4,19 @@ console.log("iniciando..." + url);
 // promesas
 fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => {
+        console.log(data)
+        const fechaCadena = data.date;
+        console.log(fechaCadena);
+        const fechaNumber = Date.parse(fechaCadena);
+        console.log(fechaNumber);
+        const fecha = new Date(fechaNumber);
+        console.log(fecha.toISOString());
+        const fechaEuropea = fecha.getDate() + "/" + (fecha.getMonth() + 1) + "/" + fecha.getFullYear();
+        console.log(fechaEuropea);
+        const spanFecha = document.getElementById("fecha");
+        spanFecha.innerText = fechaEuropea;
+    });
 
 // async function obtenerDatos() {
     // esperas
